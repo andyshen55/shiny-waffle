a06847fa23e67ab744f5673963d1302b9ff6c261
Toby <carrotglasses16@gmail.com>
2025-10-26 11:24:29 -0700

    i discriminated good and bad numbers

---
+++ cpp/problems/day_one.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <iostream>
+using namespace std;
+int main() {
+    int n = 76;
+    if (n%2 == 1){
+        cout << "oh its you... the odd one out" << endl;
+    }
+    else{
+        cout<<"why hello there my fine earth" << endl;
+    }
+}
